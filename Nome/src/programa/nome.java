1322180eac0e85148dfba651aad2c0d637efd35c
Henrique Moraes <ahmdesouza@gmail.com>
2023-11-18 01:26:46 -0300

    Create nome.java

---
+++ Nome/src/programa/nome.java
@@ -0,0 +1,14 @@
+package programa;
+
+public class nome {
+
+    public static void main(String[] args) {
+        customer();    
+    }
+
+    public static void customer() {
+        String nome = "Jose das Couves";
+        System.out.println(nome);
+
+    }
+}
