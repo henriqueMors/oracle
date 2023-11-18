db42c19b04ddc3e1bdd1a19db43cd1dc269b2f46
Henrique Moraes <ahmdesouza@gmail.com>
2023-11-18 00:54:51 -0300

    udpate

---
+++ Texto/src/programa/Texto02.java
@@ -0,0 +1,10 @@
+package programa;
+
+public class Texto02 {
+    public static void main(String[] args) {
+        System.out.println("01");
+        System.out.println("02");
+        System.out.println("03");
+        System.out.println("04");
+    }
+}
---
+++ Texto/src/programa/texto03.java
@@ -0,0 +1,16 @@
+package programa;
+
+public class texto03 {
+
+    public static void drawLegs() {
+        System.out.println("    ||  ||  ");
+        System.out.println("    ||  ||  ");
+        System.out.println("   (||)(||)  ");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("    ^   ^   ");
+        drawLegs();
+    }
+    
+}
