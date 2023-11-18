a27e05069a8009ef11c94a1c1309691f35ff8ced
Henrique Moraes <ahmdesouza@gmail.com>
2023-11-18 01:41:39 -0300

    Create smile.java

---
+++ ASCII/src/programa/smile.java
@@ -0,0 +1,14 @@
+package programa;
+
+public class smile {
+    public static void main(String[] args) {
+        System.out.println("..####..");
+        System.out.println(".#....#.");
+        System.out.println("#.Ò..Ó.#");
+        System.out.println("#......#");
+        System.out.println("#.#..#.#");
+        System.out.println("#..##..#");
+        System.out.println(".#....#.");
+        System.out.println("..####..");
+    }
+}
