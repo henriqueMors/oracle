9844331726d849a4d3dfa5757d7150825aad7187
Henrique Moraes <ahmdesouza@gmail.com>
2023-11-17 23:57:40 -0300

    create main condominio

---
+++ Condominio/src/programa/Condominio.java
@@ -0,0 +1,8 @@
+package programa;
+
+public class Condominio {
+    public static void main(String[]args) {
+        System.out.println("Criaremos um programa para registrar moradores de um condominio.");
+    }
+    
+}
