82c42f391bcf3c7e8fca3a83234581c927d39dbe
Henrique Moraes <ahmdesouza@gmail.com>
2023-11-19 00:00:48 -0300

    updates  diversos

--- Fazenda/src/programa/poleiro.java
+++ Fazenda/src/programa/poleiro.java
@@ -12,6 +12,12 @@ public class poleiro {
         double lucroPorOvo = 0.18;
         double lucroMensalPorOvo = lucroPorOvo * mediaMensalDeOvos;
 
+        int val1 = Integer.parseInt("100");
+        int val2 = Integer.parseInt("200");
+        int result = (val1 + val2);
+        System.out.println(result);
+
+
         System.out.printf("Média de ovos diário: %.0f%n", mediaDiariaDeOvos);
         System.out.printf("Média de ovos mensal: %.0f%n", mediaMensalDeOvos);
         System.out.printf("Lucro mensal de ovos: R$%.2f%n", lucroMensalPorOvo);
