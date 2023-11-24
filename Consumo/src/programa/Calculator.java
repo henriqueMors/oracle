c663a1117a4960e129399d10aaf3b69ac5ed4fb9
Henrique Moraes <ahmdesouza@gmail.com>
2023-11-24 12:32:33 -0300

    update

---
+++ Consumo/src/programa/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.gorjeta = 0.05;
+        calc.encontreTotal();
+    }
+
+    public double gorjeta = 0.15;
+    public void printGorjeta() {
+        System.out.println(gorjeta);
+    }
+}
--- Consumo/src/programa/contaIndividual.java
+++ Consumo/src/programa/contaIndividual.java
@@ -1,31 +1,47 @@
+import javax.swing.JOptionPane;
+
 public class contaIndividual {
 
-    public static double taxa = 0.05;
-    public static double gorjeta = 0.15;
-    public static double total;
+            public double taxa = 0.05;
+            public double gorjeta = 0.15;
 
-    public static void findTotal() {
-        total =  1 + taxa + gorjeta;
-        System.out.println(total);
+    public void encontreTotal() {
+
     }
 
-    public static void main(String[] args) {
-    
+    /* public static void main(String[] args) {
+
             double pes01, pes02, pes03, pes04, pes05, pes06, pes07, pes08;
 
                 pes01 = 10;
                 double total01 = pes01*( 1 + taxa + gorjeta );
                 pes02 = 12;
+                double total02 = pes02*( 1 + taxa + gorjeta );
                 pes03 = 9;
+                double total03 = pes03*( 1 + taxa + gorjeta );
                 pes04 = 8;
+                double total04 = pes04*( 1 + taxa + gorjeta );
                 pes05 = 7;
+                double total05 = pes05*( 1 + taxa + gorjeta );
                 pes06 = 15;
+                double total06 = pes06*( 1 + taxa + gorjeta );
                 pes07 = 11;
+                double total07 = pes07*( 1 + taxa + gorjeta );
                 pes08 = 30;
+                double total08 = pes08*( 1 + taxa + gorjeta );
 
-            System.out.println(total01);
+                double totalGeral = total01 + total02 + total03 + total04 + total05 + total06 + total07 + total08;
+
+            System.out.printf("R$ %.2f\n",total01);
+            System.out.printf("R$ %.2f\n",total02);
+            System.out.printf("R$ %.2f\n",total03);
+            System.out.printf("R$ %.2f\n",total04);
+            System.out.printf("R$ %.2f\n",total05);
+            System.out.printf("R$ %.2f\n",total06);
+            System.out.printf("R$ %.2f\n",total07);
+            System.out.printf("R$ %.2f\n",total08);
+            System.out.printf("Total final é de R$ %.2f\n",totalGeral); */
 
     }
-
         
 }
