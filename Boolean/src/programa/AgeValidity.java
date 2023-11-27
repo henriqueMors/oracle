ceb49046bbba1f87032ea894df988b814b24f845
Henrique Moraes <ahmdesouza@gmail.com>
2023-11-27 16:24:45 -0300

    Update AgeValidity.java

--- Boolean/src/programa/AgeValidity.java
+++ Boolean/src/programa/AgeValidity.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class AgeValidity {
 
     public static void main(String[] args) {
-
-       
+       int idade = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe idade", 0));
+       if (idade < 18) { 
+        JOptionPane.showMessageDialog(null, "NAO PODE DIRIGIR!", null, 1);
+       }
+       else
+       {
+        JOptionPane.showMessageDialog(null, "PODE DIRIGIR!", null, 1);
+       }
     }
 }
