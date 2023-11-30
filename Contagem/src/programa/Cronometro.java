30e3e1b6a1ccf6840df6f082b41d4adac0aa770d
Henrique Moraes <ahmdesouza@gmail.com>
2023-11-29 22:48:37 -0300

    Create Cronometro.java

---
+++ Contagem/src/programa/Cronometro.java
@@ -0,0 +1,28 @@
+package programa;
+
+public class Cronometro {
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+        
+        /*
+        System.out.println("Contagem regressiva em...");
+
+        int i = 10;
+
+        System.out.println(--i);
+        System.out.println(--i);
+        System.out.println(--i);
+        System.out.println(--i);
+        System.out.println(--i);
+        System.out.println(--i);
+        System.out.println(--i);
+        System.out.println(--i);
+        System.out.println(--i);
+        System.out.println("FIRE!");
+        */
+    }
+}
