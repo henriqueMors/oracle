8f738922eb15a7c34256a17e1cb99c3b4c28e268
Henrique Moraes <ahmdesouza@gmail.com>
2023-11-29 23:01:42 -0300

    Update Cronometro.java

--- Contagem/src/programa/Cronometro.java
+++ Contagem/src/programa/Cronometro.java
@@ -3,10 +3,11 @@ package programa;
 public class Cronometro {
     public static void main(String[] args) {
         
-        
-        
-        
-        
+        for (int i = 10 ; i >= 1 ; i--) {
+            System.out.println(i + ",");
+        }
+
+        System.out.println("FIRE!");
         
         
         /*
