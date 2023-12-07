5c4a9a8ddd3a388b5737b2bc3453942c4bd77e77
Henrique Moraes <ahmdesouza@gmail.com>
2023-12-07 17:18:49 -0300

    Create PrisonTest.java

---
+++ Prisioneiro/src/programa/PrisonTest.java
@@ -0,0 +1,11 @@
+package programa;
+
+public class PrisonTest {
+    public String nome;
+    public double altura;
+    public int sentenca;
+
+    public void think() {
+        System.out.println("olá");
+    }
+}
